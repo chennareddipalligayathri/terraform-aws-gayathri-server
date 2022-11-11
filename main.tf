32cc20dedf9da5bfd836ea52cef7ba6c66cf7972
chennareddipalligayathri <77479095+chennareddipalligayathri@users.noreply.github.com>
2022-11-11 12:42:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,7 @@
-resource "aws_vpc" "myvpc" {
-  tags = {
-    Name = "Gayathri-VPC"
-  }
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_subnet" "mysubnet" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "private-Gayathri"
-  }
-}
+
 resource "aws_instance" "myinstance" {
   instance_type = var.hw
   ami           = var.ami
-  subnet_id     = aws_subnet.mysubnet.id
   tags = {
     Name = var.servername
     env  = "test"
