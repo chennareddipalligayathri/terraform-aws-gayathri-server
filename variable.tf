28287bb203c15596925375230d4fd429a62d6707
chennareddipalligayathri <77479095+chennareddipalligayathri@users.noreply.github.com>
2022-11-11 14:02:43 +0530

    fixing errors

--- variable.tf
+++ variable.tf
@@ -9,20 +9,3 @@ variable "hw" {
 variable "servername" {
   type = string
 }
-variable "bucket_name" {
-  type = string
-  default ="gayathri-main-module-bucket"
-}
-resource "aws_vpc" "myvpc" {
-  tags = {
-    Name = "Gayathri-VPC"
-  }
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_subnet" "mysubnet" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "private-Gayathri"
-  }
-}
